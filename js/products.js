8588e6d7ba1cbf4e54e7c0f668dbde4be8cae8fe
Romina Mazzola <68865210+romimazzola@users.noreply.github.com>
2020-08-15 12:22:05 -0300

    Pueba

--- js/products.js
+++ js/products.js
@@ -1,13 +1,6 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
-getJSONData(PRODUCTS_URL).then(function (products){
-    if(products.status === "ok"){
-        productsArray = products.data;
-    }
-})
-});
 
 function showProductsList(array){
 
