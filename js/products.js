51950ebd80143a4038d3b4b56cd268ffdf1f680a
Romina Mazzola <68865210+romimazzola@users.noreply.github.com>
2020-08-14 21:41:02 -0300

    Agregando productos

--- js/products.js
+++ js/products.js
@@ -2,5 +2,50 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+getJSONData(PRODUCTS_URL).then(function (products){
+    if(products.status === "ok"){
+        productsArray = products.data;
+    }
+}
 });
+
+function showProductsList(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let product = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + product.imgSrc + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ product.name +`</h4>
+                        <small class="text-muted">
+                        <b> + product.currency + "$" + product.cost + </b><br>
+                        ` + product.soldCount + ` artículos vendidos</small>
+                    </div>
+                ${product.description}
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(e){
+    showSppiner();
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            array = resultObj.data;
+            showProductsList(array);
+        }
+        hideSpinner();
+    };
+};
