95c1f87dbfe24e25c9d09c0bf87231fc43b27edd
Romina Mazzola <68865210+romimazzola@users.noreply.github.com>
2020-10-11 21:58:02 -0300

    Entrega

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,41 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+var articles=[];
+//Muestra los artículos
+function showArticles(array) {
+    let htmlContentToAppend = "";
 
+    for(let i = 0; i < array.length; i++){
+        let articulos = array[i];
+        let cantidad = articulos.count;
+        let precioTotal = articulos.unitCost * cantidad;
+
+            htmlContentToAppend += `
+            <tr>
+            <td><img src=${articulos.src}></td>
+            <td>${articulos.name}</td>
+            <td>${articulos.unitCost}USD</td>
+            <td><input type="number" class="form-control" id="barra" placeholder="" value=${cantidad} min="0"></td>
+            <td id="cambiar">${precioTotal}USD</td>
+            </tr>
+            `;
+
+        document.getElementById("articlesList").innerHTML = htmlContentToAppend;
+//Cambia total a pagar
+        document.getElementById("barra").addEventListener("change", function() {
+            cantidad = this.value;
+            precioTotal = articulos.unitCost * cantidad;
+            
+            document.getElementById("cambiar").innerHTML = precioTotal + "USD";
+        });
+};
+};
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(CART_INFO_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            articles = resultObj.data;
+            showArticles(articles["articles"]);
+        };
+    });
 });
+
